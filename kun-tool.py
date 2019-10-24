22adaf8f3be4d64c2d2e642b79ef750182323f78
darkL3gionS <56880933+darkL3gionS@users.noreply.github.com>
2019-10-24 10:24:12 +0700

    Create kun-tool.py

---
+++ kun-tool.py
@@ -0,0 +1,59 @@
+from getpass import getpass
+import os
+import time
+
+def menu():
+      while True:
+           print("")
+           os.system("clear")
+           print('\033[1;36;40m<─────────────── 0000 ───────────────>')
+           print('')
+           os.system('date | lolcat')
+           print('')
+           os.system('figlet mr.41l | lolcat')
+           print("\033[1;93m")
+           print("  <═══════════[ buos pl cukk ]══════════>")
+           print("  <─[ Silahkan isi username dan password ]─>")
+           print("")
+           try:
+                x = str(input('\033[1;92mUsername \033[1;93m: '))
+                print("")
+                e = getpass('\033[1;92mPassword \033[1;93m: ')
+                print ("")
+                if x=="END" and e=="TOOTH":
+                   print('wait...')
+                   time.sleep(1)
+                   os.system('clear')
+                   print('')
+                   os.system('figlet mr.41l_kun-tools | lolcat')
+                   print('\033[1;92m ───────────────────── ')
+                   print("")
+                   time.sleep(2)
+                   break
+                else:
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("\033[1;91m     SALAH ASWW ಠ益ಠ")
+                      time.sleep(2)
+                      print("")
+                      except Exception:
+
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("\033[1;91m     SALAH ASWW ಠ益ಠ")
+                      time.sleep(2)
+           except KeyboardInterrupt:
+                      print("")
+                      os.system('killall -9 com.termux')
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("\033[1;91m     SALAH ZEYENGG ಠ﹏ಠ")
+                      time.sleep(2)
+menu()
